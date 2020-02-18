5fcd8c850a39f121af74de081bd438e2ed1e8576
Dario <dariopassariello@gmail.com>
2020-02-17 23:19:46 -0800

    fix Matomo

--- script/javascript.js
+++ script/javascript.js
@@ -300,6 +300,25 @@ SOFTWARE.
 		// FIRST TRIGGER
 		//**********************************	
 		$( document ).trigger('hashchange');
+		
+		
+		// MATOMO
+		//**********************************
+		
+		var _paq = window._paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function(){
+			var u="//www.biglogic.ca/stat/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '7']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; 
+			g.async=true; 
+			g.defer=true; 
+			g.src=u+'matomo.js'; 
+			s.parentNode.insertBefore(g,s);
+		})();
 
 	};
 	
@@ -316,24 +335,6 @@ SOFTWARE.
 		window.addEventListener("load", function(){ Exec();	} ,false);
 	}
 
-	/**************************************************************************/
-
-	// MATOMO
-
-	var _paq = window._paq || [];
-	_paq.push(['trackPageView']);
-	_paq.push(['enableLinkTracking']);
-	(function(){
-		var u="//www.biglogic.ca/stat/";
-		_paq.push(['setTrackerUrl', u+'matomo.php']);
-		_paq.push(['setSiteId', '7']);
-		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-		g.type='text/javascript'; 
-		g.async=true; 
-		g.defer=true; 
-		g.src=u+'matomo.js'; 
-		s.parentNode.insertBefore(g,s);
-	})();
 
 
 
