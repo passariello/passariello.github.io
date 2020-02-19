4c545d8f658feb88eb6d9a5d8ca20911b6b8877c
Dario <dariopassariello@gmail.com>
2020-02-18 20:11:23 -0800

    Matomo

--- script/javascript.js
+++ script/javascript.js
@@ -321,12 +321,14 @@ SOFTWARE.
 	// MATOMO
 
 	var _paq = window._paq || [];
-	_paq.push(['trackPageView']);
-	_paq.push(['enableLinkTracking']);
 	(function(){
 		var u="//www.biglogic.ca/stat/";
 		_paq.push(['setTrackerUrl', u+'matomo.php']);
 		_paq.push(['setSiteId', '7']);
+		_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+		_paq.push(["setCookieDomain", "*.https;"]);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
 		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 		g.type='text/javascript'; 
 		g.async=true; 
